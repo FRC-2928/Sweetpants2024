bfb0ec37eb1eb16ea0202c387d13e81b5b721a51
Dennis Marro <dennismarro@earthlink.net>
2024-01-14 21:49:52 -0800

    Revert to Pigeon 1

--- src/main/java/frc/robot/sensors/Pigeon.java
+++ src/main/java/frc/robot/sensors/Pigeon.java
@@ -1,21 +1,25 @@
 package frc.robot.sensors;
 
-import com.ctre.phoenix6.hardware.Pigeon2;
+// import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix.sensors.PigeonIMU;
 
-import frc.robot.Constants;
+//import frc.robot.Constants;
+import frc.robot.Constants.CANBusIDs;
 
 /**
  * Utility class responsible for the gyro
  */
 public class Pigeon{
-    private Pigeon2 m_pigeon;
+    private PigeonIMU m_pigeon;
 
     public Pigeon(){
-        m_pigeon = new Pigeon2(Constants.CANBusIDs.kPigeonIMU);
+        m_pigeon = new PigeonIMU(CANBusIDs.kPigeonIMU);
     }
 
     public double getYaw(){
-        return m_pigeon.getYaw().getValue();
+        double ypr[] = {0, 0, 0};
+        m_pigeon.getYawPitchRoll(ypr);
+        return ypr[0];
     }
 
     public void resetGyro(){
